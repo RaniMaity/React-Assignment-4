e78ddab819f4e4e4b73785488792fadfd2659a2b
Rani Maity <maityrani299@gmail.com>
2026-06-02 11:14:44 +0530

    fix: update interface import and build issues

--- src/context/form/FormContext.tsx
+++ src/context/form/FormContext.tsx
@@ -1,6 +1,7 @@
 import { createContext } from "react";
-import type { FormState } from "../Services/typescript/interface/Form.interface";
-import type { FormActionType } from "../Services/typescript/type/form.type";
+import type { FormActionType } from "../../Services/typescript/type/form.type";
+import type { FormState } from "../../Services/typescript/interface/Form.interface";
+
 
 export type FormContextType = {
   state: FormState;
